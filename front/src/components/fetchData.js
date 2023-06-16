effd620ed96013fa8f91873a203118cc7faff5f2
El mehdi Elmohammady <eelmoham@e2r12p1.1337.ma>
2023-06-16 23:38:57 +0100

    done

--- front/src/components/fetchData.js
+++ front/src/components/fetchData.js
@@ -1,4 +1,3 @@
-import { log } from 'logrocket';
 import React, { useEffect, useState } from 'react';
 // import { useHistory } from 'react-router-dom';
 import { Link } from 'react-router-dom';
@@ -28,7 +27,7 @@ const ObjectList = () => {
       <ul>
         {objects.map((object) => (
           <li key={object.id} className="flex items-center bg-gray-200 py-4 px-6">
-            <h2 className="text-lg text-gray-800 mr-auto"><strong>Semaine:</strong> {object.semaine}   ,   <strong>Matricule 1:</strong> {object.matricule_1} ... </h2>
+            <h2 className="text-lg text-gray-800 mr-auto"><strong>Semaine:</strong> {object.semaine}   ,   <strong>N° courbe:</strong> {object.n_courbe} ... </h2>
             <Link className="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded" to={{ pathname: '/DestinationPage', state: { object: object }, }}  >
              See Details
             </Link>
